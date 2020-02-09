98fa69e37d2f88952f3edf402714a81f4b3ad626
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-02-08 23:58:10 -0600

    Update run_pip_check.py

--- code/run_pip_check.py
+++ code/run_pip_check.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         pkgs = ['libcfgraph/artifacts/' + sys.argv[1]]
     else:
-        pkgs = sorted(glob.glob('libcfgraph/artifacts/*'))
+        pkgs = glob.glob('libcfgraph/artifacts/*')
 
     tot = len(pkgs)
     tot_tried = 0
